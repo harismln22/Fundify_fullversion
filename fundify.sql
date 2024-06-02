17146ec1d8defb4461668b772a40a1f98c263d5d
Hris <harismln34@gmail.com>
2024-06-02 19:24:09 +0700

    replace sql

--- fundify.sql
+++ fundify.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 01, 2024 at 02:04 PM
+-- Generation Time: Jun 02, 2024 at 02:23 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -33,15 +33,21 @@ CREATE TABLE `akun` (
   `username` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `email` varchar(100) NOT NULL,
-  `jabatan` varchar(25) NOT NULL
+  `jabatan` varchar(25) NOT NULL,
+  `role` tinyint(1) NOT NULL DEFAULT 0,
+  `tanggal` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `akun`
 --
 
-INSERT INTO `akun` (`id_akun`, `fullname`, `username`, `password`, `email`, `jabatan`) VALUES
-(18, 'nufald aha', 'aha', '$2y$10$oxVRDgPoDA953eh2YnRGeeTMub4VLtAQiFHjvu6k1PFMw6XHzDSwy', 'aha@gmail.com', 'exampel');
+INSERT INTO `akun` (`id_akun`, `fullname`, `username`, `password`, `email`, `jabatan`, `role`, `tanggal`) VALUES
+(18, '', 'aha', '$2y$10$oxVRDgPoDA953eh2YnRGeeTMub4VLtAQiFHjvu6k1PFMw6XHzDSwy', 'aha@gmail.com', '', 1, '2024-06-02 12:58:58'),
+(24, 'Maul Naufaldy', 'Manaf', '$2y$10$up.TdLWS66PeRNjLHJXxT.X.SVodK815KF1s63vRqWZuSTC2nmIXi', 'maulf@gmail.com', 'gg', 0, '2024-06-02 16:37:08'),
+(27, 'Haris MAul', 'Haris', '$2y$10$q0o3hQfCixB0Yv77hBorcuCv0UDplXpwsEWDhff2xudv8LQG9e.Fa', 'haris@gmail.com', 'lalala', 0, '2024-06-02 17:36:48'),
+(28, 'admin p', 'admin1', '$2y$10$oiQs7Qm1ZviavwqTi2/f.u1fOV2Cx7kbgRHnizY8guepJ.frYB/YS', 'admin@gmail.com', 'admin', 1, '2024-06-02 12:08:30'),
+(29, 'userr', 'user', '$2y$10$XsmZem382uDucf/Ma.r2hu39NZLEUWy2aFfUw4TwPTwUj1h3z3/tu', 'user@gmail.com', 'npc', 0, '2024-06-02 19:03:10');
 
 -- --------------------------------------------------------
 
@@ -57,6 +63,13 @@ CREATE TABLE `pemasukan` (
   `sumber` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `pemasukan`
+--
+
+INSERT INTO `pemasukan` (`id_pemasukan`, `id_akun`, `tanggal`, `jumlah`, `sumber`) VALUES
+(38, 28, '2024-06-02 11:39:43', 70000.00, 'k');
+
 -- --------------------------------------------------------
 
 --
@@ -69,6 +82,13 @@ CREATE TABLE `pengeluaran` (
   `tanggal` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `jumlah` decimal(10,2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `pengeluaran`
+--
+
+INSERT INTO `pengeluaran` (`id_pengeluaran`, `id_akun`, `tanggal`, `jumlah`) VALUES
+(12, 28, '2024-06-02 12:02:23', 70000.00);
 
 -- --------------------------------------------------------
 
@@ -124,19 +144,19 @@ ALTER TABLE `transaksi`
 -- AUTO_INCREMENT for table `akun`
 --
 ALTER TABLE `akun`
-  MODIFY `id_akun` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `id_akun` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 
 --
 -- AUTO_INCREMENT for table `pemasukan`
 --
 ALTER TABLE `pemasukan`
-  MODIFY `id_pemasukan` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id_pemasukan` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
 
 --
 -- AUTO_INCREMENT for table `pengeluaran`
 --
 ALTER TABLE `pengeluaran`
-  MODIFY `id_pengeluaran` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id_pengeluaran` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `transaksi`
